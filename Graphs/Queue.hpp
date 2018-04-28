f16090348ad6a2367b5f849d39c71e657ef2d4a5
Ryan Slipher <ryan101794@gmail.com>
2018-04-28 17:44:51 -0400

    Update Queue.hpp
    added queue

--- Graphs/Queue.hpp
+++ Graphs/Queue.hpp
@@ -1,1 +1,30 @@
-#pragma once
+//
+//Queue.hpp
+//
+
+#ifndef Queue_hpp
+#define Queue_hpp
+
+#include <string>
+#include "Node.hpp"
+
+class Queue {
+
+private:
+	Node * _head;
+	Node* _tail;
+	int _size;
+
+public:
+	Queue();
+
+	void enQueue(Node* newNode);
+	void enQueue(int VerticeID);
+	void deQueue(int VerticeID);
+	Node* find(int VerticeID);
+	void print();
+	int getSize();
+
+
+};
+#endif // !Queue_hpp
