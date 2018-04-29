31599a2b920b48f7a8b2d5feb693483ff300962c
Ryan Slipher <ryan101794@gmail.com>
2018-04-29 18:13:31 -0400

    Update Graph.cpp
    Added BFS

--- Graphs/Graph.cpp
+++ Graphs/Graph.cpp
@@ -5,12 +5,14 @@
 #include <iostream>
 #include "Graph.hpp"
 #include "DLList.hpp"
+#include "Queue.hpp"
 
 using namespace std;
 
 Graph::Graph(int size) {
 	_size = size;
 	_listArray = new DLList[_size];
+
 
 }
 
@@ -29,6 +31,24 @@ void Graph::print() {
 	}
 }
 
-void Graph::BFS() {
-
+void Graph::BFS(int s) {
+	bool * visited = new bool[_size];
+	for (int i = 0; i < _size; i++) {
+		visited[i] = false;
+		_listArray[i].enQueue(i);
+		visited[s] = true;
+		enQueue(s);
+		_listArray[i];
+	}
+	while (_size != NULL) {
+		s = _head;
+		cout << s << " ";
+		deQueue(s);
+		for (i = _listArray[adjVertexID].begin(); i != _listArray[adjVertexID].end(); ++i) {
+			if (!visited[*i]) {
+				visited[*i] = true;
+				enQueue(*i);
+			}
+		}
+	}
 }
