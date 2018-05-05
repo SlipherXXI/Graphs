2e1c980c6270cf60260754eeb1ff91b279b708b8
Ryan Slipher <ryan101794@gmail.com>
2018-05-05 15:20:01 -0400

    fixed dequeue

--- Graphs/Queue.cpp
+++ Graphs/Queue.cpp
@@ -6,82 +6,74 @@
 #include <string>
 #include "Queue.hpp"
 #include "Node.hpp"
+#include "DLList.hpp"
 
 using namespace std;
 
+//default constructure
 Queue::Queue() {
+	
 	_head = NULL;
 	_tail = NULL;
 	_size = 0;
 }
 
-void Queue::enQueue(Node* newNode) {
-	if (_tail == NULL) {
-		_tail = _head;
-		_head = newNode;
-		_tail = newNode;
+Queue::~Queue() {
+	delete _head;
+}
 
+//Methods
+bool Queue::isEmpty() {
+	if (_head == NULL) {
+		cout << "Queue is empty" << endl;
+	}
+	return true;
+}
+
+void Queue::enQueue(int VerticeID) {
+	Node * temp = new Node(VerticeID);
+	
+	if (_head == NULL) {
+		_head = _tail;
+		_tail = temp;
 	}
 	else {
-		newNode->setPrev(_tail);
-		_tail->setNext(newNode);
-		_tail = newNode;
+		temp->setNext(_tail);
+		_tail = temp;
 	}
 	_size++;
 }
 
-void Queue::enQueue(int VerticeID) {
-	Node* newNode = new Node(VerticeID);
-	enQueue(newNode);
-}
-
-int Queue::deQueue(int VerticeID) {
-	Node* trash;
-	trash = _head;
-	_head = _head->getNext();
-	delete trash;
-	_size--;
-	return VerticeID;
-}
-
-Node* Queue::find(int VerticeID) {
+int Queue::deQueue() {
+	Node *temp = new Node();
+	int value = temp->getVerticeID();
 	if (_head == NULL) {
-		return NULL;
+		cout << "\nQueue is Emtpty\n";
 	}
 	else {
-		Node* currentPtr = _head;
-		while (currentPtr != NULL) {
-			//cout << "current" << endl;
-			//cout << "key:" << currentPtr->getKey() << endl;
-			//cout << "test key:" << key << endl;
-
-			if (currentPtr->getVerticeID() == VerticeID) {
-				return currentPtr;
-			}
-			//update
-			currentPtr = currentPtr->getNext();
-		}
-		return NULL;
+		temp = _head;
+		value = temp->getVerticeID();
+		_head = _head->getNext();
+		delete temp;
 	}
+	return value;
 }
 
-void Queue::print() {
-	cout << "List contains" << endl;
+void Queue::display() {
+	Node* newNode = new Node;
+	newNode = _head;
 	if (_head == NULL) {
-		cout << "list is empty" << endl;
+		cout << "Nothing in Queue" << endl;
 	}
 	else {
-		Node* _current = _head;
-		while (_current != NULL) {
-			//cout << "ptr address "  << _current << endl;
-			cout << "(" << _current->toString() << ")" << " --> ";
-			//update
-			_current = _current->getNext();
+		while (newNode != NULL) {
+			cout << newNode->getVerticeID() << endl;
+			newNode = newNode->getNext();
 		}
-		cout << 0 << endl;
 	}
 }
 
 int Queue::getSize() {
 	return _size;
+	
 }
