68581c47446a4183b78aab0c5d797866f7f5007d
Ryan Slipher <ryan101794@gmail.com>
2018-05-05 18:43:23 -0400

    change in to out

--- Graphs/Main.cpp
+++ Graphs/Main.cpp
@@ -17,50 +17,40 @@ using namespace std;
 int main(int argc, const char* argv[]) {
 
 	string line;
-	ifstream inFile;
-	inFile.open("C:\\Users\\rslip\\source\\repos\\Graphs\\GraphsProject.txt");
-	if (!inFile) {
+	ifstream outFile;
+	outFile.open("C:\\Users\\rslip\\source\\repos\\Graphs\\GraphsProject.txt");
+	if (!outFile) {
 		cout << "Unable to open GraphsProjext.txt" << endl;
 		exit(1);
 	}
-	while (getline(inFile,line)) {
-		
-		cout << line << endl;
-
-		DLList* myList = new DLList();
-		myList->add(0);
-		myList->add(1);
-		myList->add(2);
-		myList->add(3);
-		myList->add(4);
-		myList->add(5);
-		myList->print();
-		
-		Graph myGraph(6);
-		myGraph.addEdge(5, 1);
-		
-		myGraph.addEdge(1, 3);
-		
-		myGraph.addEdge(1, 0);
-		
-		myGraph.addEdge(0, 3);
-	
-		myGraph.addEdge(0, 2);
-		
-		myGraph.addEdge(4, 0);
-
-		myGraph.addEdge(4, 3);
+	while (getline(outFile, line)) {
 
 
-		myGraph.BFS(3);
 	}
-	inFile.close();
+	Graph myGraph(6);
+	myGraph.addEdge(5, 1);
 
+	myGraph.addEdge(1, 3);
+
+	myGraph.addEdge(1, 0);
+
+	myGraph.addEdge(0, 3);
+
+	myGraph.addEdge(0, 2);
+
+	myGraph.addEdge(4, 0);
+
+	myGraph.addEdge(4, 3);
+
+
+	myGraph.BFS(3);
 	
 
-	
 
-	
+
+
+
+
 
 	cin.get();
 	return 0;
