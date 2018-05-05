28f45a24616097a9bed96b9a5931b9ff0dcd4737
Ryan Slipher <ryan101794@gmail.com>
2018-05-05 15:24:27 -0400

    Bfs still not working

--- Graphs/Graph.cpp
+++ Graphs/Graph.cpp
@@ -9,15 +9,16 @@
 
 using namespace std;
 
+//default constructure
 Graph::Graph(int size) {
 	_size = size;
 	_listArray = new DLList[_size];
-
-
 }
 
+//Methods
 void Graph::addEdge(int VerticeID, int adjVertexID) {
 	_listArray[VerticeID].add(adjVertexID);
+	
 }
 
 int Graph::getSize() {
@@ -33,19 +34,22 @@ void Graph::print() {
 
 void Graph::BFS(int s) {
 	Queue bfsQueue;
-	bfsQueue.enQueue(s);
+	
 	bool * visited = new bool[_size];
 	for (int i = 0; i < _size; i++) {
 		visited[i] = false;
 	}
 		visited[s] = true;
-	
-	while (bfsQueue.getSize() > 0) {
-		s = bfsQueue.deQueue(0);
-		cout << s << " ";
+		bfsQueue.enQueue(s);
+		cout << s << ":" << endl;
+	while (bfsQueue.getSize() != NULL) {
+		s = bfsQueue.deQueue();
+		cout << s << " " << endl;
 		for (int i = _listArray[s].getSize(); i != _listArray[s].getSize(); i++) {
 			if (!visited[i]) {
+				bfsQueue.enQueue(i);
 				visited[i] = true;
+				cout << "Visited:" << visited[i] << endl;
 				bfsQueue.enQueue(i);
 			}
 		}
